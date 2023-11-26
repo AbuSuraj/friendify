e5654619e754cd927e9ed925ef35f01cabeac37c
Abu Suraj <abusurajbd@gmail.com>
2023-11-26 15:53:52 +0600

    add login & registration form

---
+++ src/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './home.scss'
+const Home = () => {
+    return (
+        <div>
+           <p className='p'>home</p> 
+        </div>
+    );
+};
+
+export default Home;
