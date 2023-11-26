7c07928931940fa83c27c4e7db5847e9ce3c156c
Abu Suraj <abusurajbd@gmail.com>
2023-11-27 05:18:28 +0600

    connect db

--- Server/controllers/auth.controllers.js
+++ Server/controllers/auth.controllers.js
@@ -1,35 +1,35 @@
-// import { db } from "../connect.js";
-// import bcrypt from "bcryptjs";
+import { db } from "../utils/db.connect.js"; 
+import bcrypt from "bcryptjs";
 // import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
   //CHECK USER IF EXISTS
 
-//   const q = "SELECT * FROM users WHERE username = ?";
+  const q = "SELECT * FROM users WHERE username = ?";
 
-//   db.query(q, [req.body.username], (err, data) => {
-//     if (err) return res.status(500).json(err);
-//     if (data.length) return res.status(409).json("User already exists!");
-//     //CREATE A NEW USER
-//     //Hash the password
-//     const salt = bcrypt.genSaltSync(10);
-//     const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.status(500).json(err);
+    if (data.length) return res.status(409).json("User already exists!");
+    //CREATE A NEW USER
+    //Hash the password
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
 
-//     const q =
-//       "INSERT INTO users (`username`,`email`,`password`,`name`) VALUE (?)";
+    const q =
+      "INSERT INTO users (`username`,`email`,`password`,`name`) VALUE (?)";
 
-//     const values = [
-//       req.body.username,
-//       req.body.email,
-//       hashedPassword,
-//       req.body.name,
-//     ];
+    const values = [
+      req.body.username,
+      req.body.email,
+      hashedPassword,
+      req.body.name,
+    ];
 
-//     db.query(q, [values], (err, data) => {
-//       if (err) return res.status(500).json(err);
-//       return res.status(200).json("User has been created.");
-//     });
-//   });
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("User has been created.");
+    });
+  });
 };
 
 export const login = (req, res) => {
